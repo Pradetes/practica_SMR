24cdf5e6c6a3da3f190bba424103d9fa63d9ecee
Pradetes <japrados@gmail.com>
2025-03-07 23:28:06 +0100

    Primera versión

---
+++ js/validacion.js
@@ -0,0 +1,15 @@
+<script>
+    document.querySelector("form").addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        if (!name || !email || !message) {
+            alert("Por favor, completa todos los campos.");
+        } else {
+            alert("¡Formulario enviado exitosamente!");
+        }
+    });
+</script>
